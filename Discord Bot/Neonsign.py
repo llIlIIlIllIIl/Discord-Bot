eb5310d58c7a447c1af0ef2fcdc64218ed68c9b7
Jewoo-Ryu <83021646+Jewoo-Ryu@users.noreply.github.com>
2021-10-12 18:31:40 +0900

    Neonsign

---
+++ Discord Bot/Neonsign.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+from discord import Colour
+import asyncio
+from discord.ext import tasks
+
+app = commands.Bot(command_prefix='/')
+
+@app.event
+async def on_ready():
+    print(f'{app.user.name} 연결 완료!')
+    print(app.guilds)
+    #892724718958956565
+    g = app.guilds[1]
+    
+    #print(g.roles)
+    r = g.roles[1]
+    
+    await app.change_presence(status=discord.Status.online, activity=None)    
+    
+@app.command()
+async def change_color(ctx):
+    g = app.guilds[1]
+    r = g.roles[1]
+    if neonsign_nickname.is_running():
+        neonsign_nickname.stop()
+    else:
+        neonsign_nickname.start(r)
+        
+@tasks.loop(seconds=1)
+async def neonsign_nickname(role):
+    await role.edit(colour=Colour.random())
+     
+app.run('ODkyNzIzODY5NTYyMDUyNjI4.YVRD5A.5SrVawYTuD1Ir7IRpoMf7VyQ9I8')
