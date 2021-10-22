721bca52dd0f4ff94eaf0b6f2d857701ae6c3573
Jewoo-Ryu <83021646+Jewoo-Ryu@users.noreply.github.com>
2021-10-22 21:31:31 +0900

    Update Server.py

--- Bruteforce Attack/Server.py
+++ Bruteforce Attack/Server.py
@@ -1,13 +1,20 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
+import urllib.parse as parse
 
 class ServerHandler(BaseHTTPRequestHandler):
     
     def do_GET(self):
-        self.send_response(200)
+        self.send_response(200) # 연결 성공 메시지 출력.
+
         self.send_header('content-type','text/html') # <head>
         self.end_headers() # </head>
-        self.wfile.write('hello world'.encode())
+
+        self.wfile.write('<p>hello world</p>'.encode()) # text
+        self.wfile.write(self.path.encode()) # '/'
+
+        self.wfile.write('<br>'.encode())
+        if '?' in self.path:
+            print(dict(parse.parse_qsl(self.path.split('?')[1]))) # 딕셔너리 형태로 '?' 뒤 값들을 & 단위로 출력.
 
     def do_POST(self):
         pass
