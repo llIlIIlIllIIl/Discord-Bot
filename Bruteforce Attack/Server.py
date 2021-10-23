601615cf1bc126ee73251e55e3258482774853db
ryujewoo <83021646+ryujewoo@users.noreply.github.com>
2021-10-23 23:09:57 +0900

    Update Server.py

--- Bruteforce Attack/Server.py
+++ Bruteforce Attack/Server.py
@@ -1,5 +1,5 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import urllib.parse as parse
+import urllib.parse as parser
 
 class ServerHandler(BaseHTTPRequestHandler):
     
@@ -9,15 +9,33 @@ class ServerHandler(BaseHTTPRequestHandler):
         self.send_header('content-type','text/html') # <head>
         self.end_headers() # </head>
 
-        self.wfile.write('<p>hello world</p>'.encode()) # text
+        self.wfile.write('<p> hello world get </p>'.encode()) # text
+        print('get 요청이 들어왔습니다.')
+
         self.wfile.write(self.path.encode()) # '/'
 
         self.wfile.write('<br>'.encode())
         if '?' in self.path:
-            print(dict(parse.parse_qsl(self.path.split('?')[1]))) # 딕셔너리 형태로 '?' 뒤 값들을 & 단위로 출력.
+            self.wfile.write(str(self.path.split('?')[1].split('&')).encode())
+            print(parser.parse_qsl(self.path.split('?')[1]))
+            print(dict(parser.parse_qsl(self.path.split('?')[1]))) # 딕셔너리 형태로 '?' 뒤 값들을 & 단위로 출력.
 
     def do_POST(self):
-        pass
+        self.send_response(200) # 연결 성공 메시지 출력.
+
+        self.send_header('content-type','text/html') # <head>
+        self.end_headers() # </head>
+
+        self.wfile.write('<p> hello world post </p>'.encode()) # text
+        print('post 요청이 들어왔습니다.')
+
+        self.wfile.write(self.path.encode()) # '/'
+        self.wfile.write('<br>'.encode())
+        
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        if data is not None:
+            data_decode = dict(parser.parse_qs(data.decode()))
+        print(f'post params = {data_decode}')
 
 PORT = 8080
 server = HTTPServer(('', PORT), ServerHandler)
